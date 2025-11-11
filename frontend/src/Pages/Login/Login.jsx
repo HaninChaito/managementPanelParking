9cd0ab88e14df7e2c3c5675f2a377bd17187e59e
HaninChaito <haninchaito@gmail.com>
2025-11-11 03:32:47 +0200

    pp

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "${import.meta.env.VITE_API_URL}/register/login",
+        '${import.meta.env.VITE_API_URL}/register/login',
         credentials
       );
       if (response.data.msg === "success") {
